01f85e6e4b2d6975893dc617958314e66e22a921
Tristin <charismental@gmail.com>
2019-11-18 16:37:04 -0800

    Setup vuetify dark theme

--- client/src/plugins/vuetify.ts
+++ client/src/plugins/vuetify.ts
@@ -4,4 +4,18 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        dark: true,
+        themes: {
+            dark: {
+                primary: '#007f97',
+                secondary: '#a4bcbc',
+                accent: '#7c587f',
+                info: '#4c3f77',
+                warning: '#051126'
+                // error: '',
+                // success: ''
+            }
+        }
+    }
 });
