09c3ec4ff0f0b5884a58ce53320c33d825c006c6
Tristin <charismental@gmail.com>
2019-11-21 17:28:22 -0800

    Display avatar upon signin

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,34 @@
 <template>
   <v-app>
-    <v-navigation-drawer app temporary fixed v-model="sideNav">
-      <v-toolbar color="primary" dark text>
-        <v-app-bar-nav-icon @click="toggleSideNav"></v-app-bar-nav-icon>
-        <router-link to="/" tag="span" style="cursor: pointer">
-          <h1 class="title pl-3">GraceWayRadio</h1>
-        </router-link>
-      </v-toolbar>
+    <v-navigation-drawer
+      app
+      fixed
+      temporary
+      v-model="sideNav">
+      <div v-if="!user">
+        <v-toolbar
+          color="primary"
+          dark text>
+          <v-app-bar-nav-icon @click="toggleSideNav"></v-app-bar-nav-icon>
+          <router-link to="/" tag="span" style="cursor: pointer">
+            <h1 class="title pl-3">GraceWayRadio</h1>
+          </router-link>
+        </v-toolbar>
 
-      <v-divider></v-divider>
-
-      <v-list>
+        <v-divider></v-divider>
+      </div>
+      <v-list nav>
+          <div v-if="user">
+            <v-list-item to="/profile">
+              <v-list-item-avatar>
+                <v-img :src="user.avatar"></v-img>
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title class="title">My Profile</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-divider></v-divider>
+          </div>
         <v-list-item
           ripple
           v-for="(item, i) in navItems"
@@ -27,7 +45,6 @@
           </v-list-item-content>
         </v-list-item>
 
-        <!-- Signout Button -->
         <v-list-item v-if="user" @click="handleSignoutUser">
           <v-list-item-action>
             <v-icon>exit_to_app</v-icon>
@@ -50,8 +67,15 @@
           transition="scale-transition"
           width="50"
         />
-
-        <router-link class="mt-1 display-1 hidden-xs-only" tag="span" to="/" style="cursor: pointer">
+        <router-link
+          class="mt-1"
+          :class="{
+              'title': $vuetify.breakpoint.smAndDown,
+              'display-1': $vuetify.breakpoint.mdAndUp
+            }"
+          tag="span"
+          to="/"
+          style="cursor: pointer">
           GraceWayRadio
         </router-link>
       </div>
@@ -62,23 +86,50 @@
     </v-app-bar>
 
     <v-content>
-      <router-view></router-view>
+      <v-card color="primary" class="main-content mx-auto mt-5" width="90vw">
+        <player />
+        <transition name="fade">
+          <router-view></router-view>
+        </transition>
+      </v-card>
     </v-content>
+    <v-bottom-navigation
+      :value="activeBtn"
+      grow
+      fixed
+      background-color="accent">
+      <v-btn>
+        <span>Recents</span>
+        <v-icon>history</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Favorites</span>
+        <v-icon>favorite</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Upcoming</span>
+        <v-icon>schedule</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import Player from './components/Player'
 import { mapGetters } from 'vuex';
 
 export default Vue.extend({
   name: 'App',
 
   components: {
+    Player
   },
 
   data: () => ({
-    activeBtn: 1,
+    activeBtn: -1,
     searchTerm: '',
     sideNav: false
   }),
@@ -87,8 +138,8 @@ export default Vue.extend({
     navItems() {
       let items = [
         { icon: 'library_music', title: 'Songs', link: '/songs' },
-        { icon: 'lock_open', title: 'Sign In', link: '/signin' },
-        { icon: 'create', title: 'Sign Up', link: '/signup' }
+        { icon: 'lock_open', title: 'Log In', link: '/login' },
+        // { icon: 'create', title: 'Sign Up', link: '/signup' }
       ]
       if (this.user) {
         items = [
@@ -112,9 +163,28 @@ export default Vue.extend({
 
 <style>
 #app {
-  background-color: #a4bcbc;
+  /* background-color: #a4bcbc; */
+  background: linear-gradient(to bottom left, #4c3f77, #051126);
 }
 .v-navigation-drawer__content {
   background-color: #a4bcbc;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition-property: opacity;
+  transition-duration: 0.25s;
+}
+
+.fade-enter-active {
+  transition-delay: 0.25s;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
+.main-content {
+    border-radius: 20px !important;
+    background: linear-gradient(to bottom right, #7c587f, #4c3f77);
 }
 </style>
