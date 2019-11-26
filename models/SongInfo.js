b2353dda5828b8762a6269e9c3c9165d98c47c0f
Tristin <charismental@gmail.com>
2019-11-25 17:02:52 -0800

    Create SongInfo model

---
+++ models/SongInfo.js
@@ -0,0 +1,127 @@
+const mongoose = require("mongoose");
+
+const SongInfoSchema = new mongoose.Schema({
+  song_info: {
+    combine: {
+      type: String
+    },
+    title: {
+      type: String
+    },
+    artist: {
+      type: String
+    },
+    album: {
+      type: String
+    },
+    minsec: {
+      type: String,
+      required: true
+    },
+    duration: {
+      type: Number,
+      required: true
+    },
+    picture: {
+      type: String
+    },
+    buycd: {
+      type: String
+    },
+    songid: {
+      type: Number,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true
+    },
+    genre: {
+      type: String
+    }
+  },
+  song_history: [
+    {
+      combine: {
+        type: String
+      },
+      title: {
+        type: String
+      },
+      artist: {
+        type: String
+      },
+      album: {
+        type: String
+      },
+      minsec: {
+        type: String,
+        required: true
+      },
+      duration: {
+        type: Number,
+        required: true
+      },
+      picture: {
+        type: String
+      },
+      buycd: {
+        type: String
+      },
+      songid: {
+        type: Number,
+        required: true
+      },
+      type: {
+        type: String,
+        required: true
+      },
+      genre: {
+        type: String
+      }
+    }
+  ],
+  song_queue: [
+    {
+      combine: {
+        type: String
+      },
+      title: {
+        type: String
+      },
+      artist: {
+        type: String
+      },
+      album: {
+        type: String
+      },
+      minsec: {
+        type: String,
+        required: true
+      },
+      duration: {
+        type: Number,
+        required: true
+      },
+      picture: {
+        type: String
+      },
+      buycd: {
+        type: String
+      },
+      songid: {
+        type: Number,
+        required: true
+      },
+      type: {
+        type: String,
+        required: true
+      },
+      genre: {
+        type: String
+      }
+    }
+  ]
+});
+
+module.exports = mongoose.model("SongInfo", SongInfoSchema);
