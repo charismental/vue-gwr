8aa5bb12862b3e0d737dba439b15227a20036bce
Tristin <charismental@gmail.com>
2019-12-13 14:17:05 -0800

    cleanup / moved data/methods to store

--- client/src/components/Player.vue
+++ client/src/components/Player.vue
@@ -1,35 +1,8 @@
 <template>
-  <!-- <div>
-    <v-col cols="12">
-      <v-card color="warning">
-        <v-row>
-          <v-col sm="2">
-            <v-btn outlined color="white" large fab @click="playStream" v-if="!isPlaying">
-              <v-icon>play_arrow</v-icon>
-            </v-btn>
-            <v-btn outlined color="white" large fab @click="pauseStream" v-else>
-              <v-icon>pause</v-icon>
-            </v-btn>
-          </v-col>
-
-          <v-col sm="8">
-            <v-card-title class="headline" v-text="currentSongInfo.title"></v-card-title>
-            <v-card-subtitle v-text="currentSongInfo.artist"></v-card-subtitle>
-          </v-col>
-
-          <v-col sm="2">
-            <v-avatar class="ma-1" size="100" tile>
-              <v-img :src="imgUrl(currentSongInfo)"></v-img>
-            </v-avatar>
-          </v-col>
-        </v-row>
-      </v-card>
-    </v-col>
-  </div>-->
   <v-container>
-    <v-layout>
+    <v-layout class="mt-n2">
       <img
-        :src="imgUrl(currentSongInfo)"
+        :src="imgUrl"
         class="main-image"
         :class="{'mobile': $vuetify.breakpoint.smAndDown}"
       />
@@ -41,10 +14,10 @@
     <v-row align="center" class="controls">
       <v-col>
         <div class="text-center">
-          <v-btn text icon height="80" width="80" @click="playStream" v-if="!isPlaying">
+          <v-btn text icon height="80" width="80" @click="playPause" v-if="!isPlaying">
             <v-icon size="80">play_arrow</v-icon>
           </v-btn>
-          <v-btn text icon height="80" width="80" @click="pauseStream" v-else>
+          <v-btn text icon height="80" width="80" @click="playPause" v-else>
             <v-icon size="80">pause</v-icon>
           </v-btn>
         </div>
@@ -54,74 +27,44 @@
 </template>
 
 <script>
-import { Howl, Howler } from "howler"
 import { mapGetters } from "vuex"
 
 export default {
   name: "player",
-  data() {
-    return {
-      isPlaying: false,
-      isConnected: false
-    }
-  },
-  sockets: {
-    connect() {
-      this.isConnected = true
-    },
+  // data() {
+  //   return {
+  //     isConnected: false
+  //   }
+  // },
+  // sockets: {
+  //   connect() {
+  //     this.isConnected = true
+  //   },
 
-    disconnect() {
-      this.isConnected = false
-    },
-    updateSongInfo() {
-      this.refreshSongInfo()
-    }
-  },
+  //   disconnect() {
+  //     this.isConnected = false
+  //   },
+  //   updateSongInfo() {
+  //     this.refreshSongInfo()
+  //   }
+  // },
   computed: {
-    ...mapGetters(["currentSongInfo"]),
-    stream: () => {
-      const streamObject = new Howl({
-        src: "http://136.0.16.57:8000/.stream",
-        html5: true,
-        // eslint-disable-next-line no-console
-        onplayerror: () => console.log("play error")
-      })
-      return streamObject
-    }
+    ...mapGetters(["currentSongInfo", "stream", "isPlaying", "imgUrl"])
   },
-  created() {
-    this.$store.dispatch("getCurrentSongs")
-  },
+  // created() {
+  //   this.$store.dispatch("getCurrentSongs")
+  // },
   methods: {
-    refreshSongInfo() {
-      this.$store.dispatch("getCurrentSongs")
-      // eslint-disable-next-line no-console
-      console.log("update the song info, jimmy!")
-    },
-    playStream() {
-      if (!this.isPlaying) {
-        this.stream.play()
-        this.isPlaying = true
-      }
-    },
-    pauseStream() {
-      if (this.isPlaying) {
-        this.stream.pause()
-        this.isPlaying = false
-      }
-    },
-    imgUrl(song) {
-      const url = "https://radiomv.org/samHTMweb/"
-      if (song.picture) {
-        return url + song.picture
-      } else if (this.loading) {
-        return url + "loading.gif"
-      } else {
-        return url + "customMissing.jpg"
-      }
+    // refreshSongInfo() {
+    //   this.$store.dispatch("getCurrentSongs")
+    //   // eslint-disable-next-line no-console
+    //   console.log("update the song info, jimmy!")
+    // },
+    playPause() {
+      this.$store.dispatch('playPause')
     }
   }
-};
+}
 </script>
 
 <style>
